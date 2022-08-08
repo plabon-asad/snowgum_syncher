0b715bf76da6b50c664add6284226ba09138b03e
th-pias <pias221191@gmail.com>
2022-08-08 13:07:43 +0600

    spree adjustment model added

---
+++ app/models/base_ecommerce/spree/adjustment.rb
@@ -0,0 +1,3 @@
+class BaseEcommerce::Spree::Adjustment < BaseEcommerce::Base
+  self.table_name_prefix = 'spree_'
+end
---
+++ app/models/snowgum/spree/adjustment.rb
@@ -0,0 +1,3 @@
+class Snowgum::Spree::Adjustment < Snowgum::Base
+  self.table_name_prefix = 'spree_'
+end
